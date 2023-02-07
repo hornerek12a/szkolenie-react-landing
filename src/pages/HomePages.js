2a510bda2547b91f41689d0bf5223019d5b873ce
Michał Karaś <karasmichal34@gmail.com>
2023-02-07 19:59:45 +0100

    Nawigacja

---
+++ src/pages/HomePages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainTemplate } from '../components/MainTemplate/MainTemplate'
+
+function HomePages() {
+  return (
+    <MainTemplate>
+      <h2>Home pages</h2>
+    </MainTemplate>
+  )
+}
+
+export default HomePages
