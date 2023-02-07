653cbc33279e371a79114ee79058bb9349d5e35f
Michał Karaś <karasmichal34@gmail.com>
2023-02-07 19:41:14 +0100

    dodanie route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FeaturesPage from './components/pages/FeaturesPage';
+import HomePages from './components/pages/HomePages';
+import SigninPage from './components/pages/SigninPage';
+import TeamsPages from './components/pages/TeamsPages';
 
 const App = () => {
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>hhhdd</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePages/>}/>
+          <Route path="/features" element={<FeaturesPage/>}/>
+          <Route path="/team" element={<TeamsPages/>}/>
+          <Route path="/signin" element={<SigninPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/pages/TeamsPages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TeamsPages() {
+  return (
+    <div>TeamsPages</div>
+  )
+}
+
+export default TeamsPages
